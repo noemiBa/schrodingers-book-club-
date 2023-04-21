b5c74c5ea215bf853afc9734f683e0aa6e47e07d
noemiBa <banalnoemi@gmail.com>
2023-04-21 14:59:21 +0100

    Add vue front-end for signin/signup

---
+++ front-end/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="app">
+    <SignIn />
+</div>
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+
+export default {
+    name: 'App',
+    components: {
+        SignIn
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
