ee4a39f6652580f79a0b9f830daa2fe162aa1244
noemiBa <noemi.banal@ucdconnect.ie>
2023-04-21 14:59:21 +0100

    Add vue front-end for signin/signup

---
+++ front-end/src/components/SignIn.vue
@@ -0,0 +1,113 @@
+<template>
+<div class="container">
+    <h1>{{ title }}</h1>
+    <b-form v-if="!loggedIn" @submit.prevent="signIn">
+        <b-form-group id="username-group" label="Username:" label-for="username-input">
+            <b-form-input id="username-input" v-model="username" required placeholder="Enter your username"></b-form-input>
+        </b-form-group>
+        <b-form-group id="password-group" label="Password:" label-for="password-input">
+            <b-form-input id="password-input" v-model="password" required type="password" placeholder="Enter your password"></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary" :disabled="signInDisabled">Sign In</b-button>
+        <b-button @click="signUp" variant="secondary">Sign Up</b-button>
+    </b-form>
+    <div v-else>
+        <h3>You are logged in as {{ username }}!</h3>
+        <b-button @click="logOut" variant="primary">Log Out</b-button>
+    </div>
+</div>
+</template>
+
+<script>
+import {
+    ref
+} from 'vue'
+import {
+    BForm,
+    BFormGroup,
+    BFormInput,
+    BButton
+} from 'bootstrap-vue'
+import axios from 'axios'
+
+export default {
+    name: 'SignIn',
+    components: {
+        BForm,
+        BFormGroup,
+        BFormInput,
+        BButton
+    },
+    setup() {
+        const title = ref('Sign In')
+        const loggedIn = ref(false)
+        const username = ref('')
+        const password = ref('')
+
+        const validateInput = () => {
+            return username.value.trim() !== '' && password.value.trim() !== ''
+        }
+
+        const signIn = () => {
+            if (validateInput()) {
+                axios.post('http://localhost:3000/users', {
+                        name: username.value,
+                        password: password.value
+                    })
+                    .then(() => {
+                        loggedIn.value = true
+                    })
+                    .catch(error => {
+                        console.error(error)
+                        alert('Error signing in')
+                    })
+            }
+        }
+
+        const signUp = () => {
+            if (validateInput()) {
+                axios.post('http://localhost:3000/users', {
+                        name: username.value,
+                        password: password.value
+                    })
+                    .then(() => {
+                        alert('User created successfully') 
+                    })
+                    .catch(error => {
+                        console.error(error)
+                        alert('Error creating user')
+                    })
+            }
+        }
+
+        const logOut = () => {
+            loggedIn.value = false
+            username.value = ''
+            password.value = ''
+        }
+
+        const signInDisabled = () => {
+            return !validateInput()
+        }
+
+        return {
+            title,
+            loggedIn,
+            username,
+            password,
+            signIn,
+            signUp,
+            logOut,
+            signInDisabled
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+    max-width: 500px;
+    margin: 0 auto;
+    padding-top: 50px;
+}
+</style>
