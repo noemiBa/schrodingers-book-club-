c2662c78b663372256dc8a09e3e7dcedb4c220ae
noemiBa <noemi.banal@ucdconnect.ie>
2023-05-11 16:43:33 +0100

    Add routing to front-end

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,13 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue' 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import routes from './routes';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
 Vue.use(BootstrapVue); 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+new Vue({
+  router,
+  render: h => h('router-view')
+}).$mount('#app')
---
+++ front-end/src/routes.js
@@ -0,0 +1,7 @@
+import App from './App.vue';
+import SignIn from './components/SignIn.vue';
+
+export default [
+  { path: '/', component: App },
+  { path: '/signin', component: SignIn }
+];
