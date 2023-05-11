4223aa2aca0aace738b647c08c4a4ec39decdfd4
noemiBa <banalnoemi@gmail.com>
2023-05-11 14:08:01 +0100

    Use NodeCache module to cache response from the API calls in quotes.service

--- QuotesService/src/service/quotes.service.js
+++ QuotesService/src/service/quotes.service.js
@@ -1,5 +1,7 @@
+const NodeCache = require('node-cache');
 const request = require('request-promise');
 const openLibraryURL = 'https://openlibrary.org/'
+const cache = new NodeCache();
 
 async function getBooksWithExcerpts() {
     const isbnList = await getIsbnList();
@@ -7,7 +9,7 @@ async function getBooksWithExcerpts() {
   
     for (const isbn of isbnList) {
       const bookData = await getBookData(isbn);
-      if (bookData.excerpts !== undefined) {
+      if (bookData.excerpts) {
         const excerpts = bookData.excerpts.map(excerpt => excerpt.text);
         bookList.push({
           isbn: isbn,
@@ -21,16 +23,35 @@ async function getBooksWithExcerpts() {
 }
   
 async function getBookData(isbn) {
+    const cacheKey = `bookData:${isbn}`;
     const bookApiUrl = `${openLibraryURL}api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
-    const bookResponse = await request({ uri: bookApiUrl, json: true });
-    return bookResponse[`ISBN:${isbn}`];
+  
+    return getCachedData(cacheKey, async () => {
+      const bookResponse = await request({ uri: bookApiUrl, json: true });
+      return bookResponse[`ISBN:${isbn}`];
+    });
+}
+  
+async function getIsbnList() {
+    const cacheKey = `isbnList`;
+    const apiUrl = `${openLibraryURL}/search.json?q=harry+potter&fields=isbn`;
+  
+    return getCachedData(cacheKey, async () => {
+      const response = await request({ uri: apiUrl, json: true });
+      return response.docs.flatMap(book => book.isbn).slice(0,50);
+    });
 }
 
-async function getIsbnList() {
-    const apiUrl = `${openLibraryURL}/search.json?q=harry+potter&fields=isbn`;
-    const response = await request({ uri: apiUrl, json: true });
-    const isbnList = response.docs.flatMap(book => book.isbn).slice(0,50);
-    return isbnList;
-}
+async function getCachedData(cacheKey, requestFn) {
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+      return cachedData;
+    }
+  
+    const data = await requestFn();
+    cache.set(cacheKey, data);
+  
+    return data;
+  }
 
 module.exports = { getBooksWithExcerpts };
