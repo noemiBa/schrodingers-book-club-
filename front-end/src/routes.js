5e3e1894007ad4fe54cf955d2685d038514a279d
noemiBa <banalnoemi@gmail.com>
2023-05-13 19:06:49 +0100

    Add QuizService.vue component (only post implemented)

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,9 +1,11 @@
 import App from './App.vue';
 import SignIn from './components/SignIn.vue';
 import QuotesPage from './components/QuotesPage.vue';
+import QuizPage from './components/QuizPage.vue';
 
 export default [
   { path: '/', component: App },
   { path: '/signin', component: SignIn },
-  { path: '/blind-date', component: QuotesPage }
+  { path: '/blind-date', component: QuotesPage },
+  { path: '/quiz', component: QuizPage }
 ];
