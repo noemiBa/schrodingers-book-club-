a481505a3780f5448293a1315d1f2f51c2e17e83
noemiBa <noemi.banal@ucdconnect.ie>
2023-05-14 19:11:40 +0100

    Add vuex store to front-end to store user loggedIn status/ username. Send post request to user's recommendations when 'unveil the mystery' is clicked in QuotesPage

--- front-end/src/components/SignIn.vue
+++ front-end/src/components/SignIn.vue
@@ -1,106 +1,123 @@
 <template>
-<PageWrapper title="Sign In/Sign UP"> 
-<div class="container">
-    <b-form v-if="!loggedIn" @submit.prevent="signIn">
-        <b-form-group id="username-group" label="Username:" label-for="username-input">
+    <PageWrapper title="Sign In/Sign Up"> 
+      <div class="container">
+        <b-form v-if="!loggedIn" @submit.prevent="signIn">
+          <b-form-group id="username-group" label="Username:" label-for="username-input">
             <b-form-input id="username-input" v-model="username" required placeholder="Enter your username"></b-form-input>
-        </b-form-group>
-        <b-form-group id="password-group" label="Password:" label-for="password-input">
+          </b-form-group>
+          <b-form-group id="password-group" label="Password:" label-for="password-input">
             <b-form-input id="password-input" v-model="password" required type="password" placeholder="Enter your password"></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary" :disabled="signInDisabled">Sign In</b-button>
-        <b-button @click="signUp" variant="secondary">Sign Up</b-button>
-    </b-form>
-    <div v-else>
-        <h3>You are logged in as {{ username }}!</h3>
-        <b-button @click="logOut" variant="primary">Log Out</b-button>
-    </div>
-</div>
-</PageWrapper>
-</template>
-
-<script>
-import { ref } from 'vue'
-import { BForm, BFormGroup, BFormInput, BButton } from 'bootstrap-vue'
-import axios from 'axios'
-import PageWrapper from './PageWrapper.vue'
-
-export default {
+          </b-form-group>
+          <b-button type="submit" variant="primary" :disabled="signInDisabled">Sign In</b-button>
+          <b-button @click="signUp" variant="secondary">Sign Up</b-button>
+        </b-form>
+        <div v-else>
+           <h3>You are logged in as {{ displayUsername }}!</h3>
+          <b-button @click="logOut" variant="primary">Log Out</b-button>
+        </div>
+      </div>
+    </PageWrapper>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import { BForm, BFormGroup, BFormInput, BButton } from 'bootstrap-vue'
+  import axios from 'axios'
+  import PageWrapper from './PageWrapper.vue'
+  import store from '@/store/index.js'
+  
+  export default {
     name: 'SignIn',
     components: {
-        BForm,
-        BFormGroup,
-        BFormInput,
-        BButton,
-        PageWrapper
+      BForm,
+      BFormGroup,
+      BFormInput,
+      BButton,
+      PageWrapper
+    },
+    computed: {
+      loggedIn() {
+        return store.state.loggedIn
+      },
+      inputUsername() {
+        return store.state.inputUsername
+      },
+      displayUsername() {
+        return this.loggedIn ? this.inputUsername : this.username
+      }
     },
     setup() {
-        const title = ref('Sign In')
-        const loggedIn = ref(false)
-        const username = ref('')
-        const password = ref('')
-
-        const validateInput = () => {
-            return username.value.trim() !== '' && password.value.trim() !== ''
+      const title = ref('Sign In')
+      const username = ref('')
+      const password = ref('')
+  
+      const validateInput = () => {
+        return username.value.trim() !== '' || password.value.trim() !== ''
+      }
+  
+      const signIn = () => {
+        if (validateInput()) {
+          axios.post('http://localhost:3000/users', {
+            name: username.value,
+            password: password.value
+          })
+            .then(() => {
+                store.commit('SET_LOGGED_IN', true)
+                store.commit('SET_USERNAME', username.value)
+            })
+            .catch(error => {
+              console.error(error)
+              alert('Error signing in')
+            })
         }
-
-        const signIn = () => {
-            if (validateInput()) {
-                axios.post('http://localhost:3000/users', {
-                        name: username.value,
-                        password: password.value
-                    })
-                    .then(() => {
-                        loggedIn.value = true
-                    })
-                    .catch(error => {
-                        console.error(error)
-                        alert('Error signing in')
-                    })
-            }
+      }
+  
+      const signUp = () => {
+        if (validateInput()) {
+          axios.post('http://localhost:3000/users', {
+            name: username.value,
+            password: password.value
+          })
+            .then(() => {
+               store.commit('SET_LOGGED_IN', true)
+               store.commit('SET_USERNAME', username.value)
+            })
+            .catch(error => {
+              store.commit('SET_LOGGED_IN', true)
+              store.commit('SET_USERNAME', username.value)
+              console.error(error)
+              alert('Error creating user')
+            })
         }
-
-        const signUp = () => {
-            if (validateInput()) {
-                axios.post('http://localhost:3000/users', {
-                        name: username.value,
-                        password: password.value
-                    })
-                    .then(() => {
-                        alert('User created successfully') 
-                    })
-                    .catch(error => {
-                        console.error(error)
-                        alert('Error creating user')
-                    })
-            }
-        }
-
-        const logOut = () => {
-            loggedIn.value = false
-            username.value = ''
-            password.value = ''
-        }
-
-        const signInDisabled = () => {
-            return !validateInput()
-        }
-
-        return {
-            title,
-            loggedIn,
-            username,
-            password,
-            signIn,
-            signUp,
-            logOut,
-            signInDisabled
-        }
+      }
+  
+      const logOut = () => {
+        store.commit('SET_LOGGED_IN', false)
+        store.commit('SET_USERNAME', '')
+        username.value = ''
+        password.value = ''
+      }
+  
+      const signInDisabled = () => {
+        return !validateInput()
+      }
+  
+      return {
+        title,
+        username,
+        password,
+        signIn,
+        signUp,
+        logOut,
+        signInDisabled
+      }
     }
-}
-</script>
-
-<style>
+  }
+  </script>
+  
+  
+  
+<style>  
 .container {
     max-width: 500px;
     margin: 0 auto;
