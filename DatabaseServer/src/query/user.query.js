8420500644cf21e3f59189880aa2c015d80624fb
Simon <me@simonfarrelly.com>
2023-05-16 18:53:11 +0100

    added login functionality and theoretically a recommendation system but it is wholly untested and written by a tired simon.

--- DatabaseServer/src/query/user.query.js
+++ DatabaseServer/src/query/user.query.js
@@ -1,6 +1,7 @@
 const BOOK_QUERY = {
 	SELECT_USERS: 'SELECT * FROM users',
 	SELECT_USER_BY_ID: 'SELECT * FROM users WHERE id = ?',
+	SELECT_USER_BY_NAME: 'SELECT * FROM users WHERE username = ?',
 	CREATE_USER: 'INSERT INTO users(username, password) VALUES(?, ?)',
 	UPDATE_USER: 'UPDATE users SET username = ?, password = ? WHERE id = ?',
 	DELETE_USER: 'DELETE FROM users WHERE id = ?',
